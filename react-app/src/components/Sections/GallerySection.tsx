80fc9c3701dc7581e7ee25ff20a75838f1457ff1
CoopCodes <104402442+CoopCodes@users.noreply.github.com>
2025-09-01 13:32:28 +1000

    Moving title and button to the left

--- react-app/src/components/Sections/GallerySection.tsx
+++ react-app/src/components/Sections/GallerySection.tsx
@@ -1,4 +1,5 @@
 import type { EventModel } from '../../models/eventModel'
+import { ArrowButton } from '../ArrowButton'
 import { defaultSVGFilterTemplate, SVGFilter } from '../SVGFilter'
 
 interface GallerySectionProps {
@@ -8,25 +9,33 @@ interface GallerySectionProps {
 export function GallerySection({ events }: GallerySectionProps) {
     
     return (
-        <div className="container relative bg-textured-black min-h-[100vh] grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 auto-rows-max">
-            {events.map((event, index) => (
-                <div key={index} className="col-span-1 relative group">
-                    <img src={event.images[0]} alt={event.title} className="w-full h-full object-cover" />
-                    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 group-hover:opacity-100 transition-opacity">
-                        <div className="absolute bottom-0 left-0 right-0 p-2 sm:p-4">
-                            <SVGFilter
-                                animate={true}
-                                template={{
-                                ...defaultSVGFilterTemplate,
-                                scale: 1,
-                            }}>
-                                <h3 className="text-white text-xl sm:text-4xl tracking-[-0.04em] line-clamp-2 mb-1 leading-[100%]">{event.title}</h3>
-                                <p className="text-white arial text-sm sm:text-base">{event.date.toLocaleDateString()}</p>
-                            </SVGFilter>
+        <div className="container relative bg-textured-black h-[100vh] max-h-[100vh] grid grid-cols-1 lg:grid-cols-3 gap-4 py-10 lg:py-20">
+            <div className="flex-col gap-1.5 my-auto max-lg:order-last">
+                <SVGFilter animate={true}>
+                    <h2 className='h2 leading-none'>gallery</h2>
+                </SVGFilter>
+                <ArrowButton title="see all" href="#" />
+            </div>
+            <div className="flex flex-wrap overflow-y-hidden gap-4 col-span-2 lg:justify-end max-lg:order-first max-lg:mt-12">
+                {events.map((event, index) => (
+                    <div key={index} className="relative group">
+                        <img src={event.images[0]} alt={event.title} className="w-full h-full object-cover" />
+                        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 group-hover:opacity-100 transition-opacity">
+                            <div className="absolute bottom-0 left-0 right-0 p-2 sm:p-4">
+                                <SVGFilter
+                                    animate={true}
+                                    template={{
+                                    ...defaultSVGFilterTemplate,
+                                    scale: 1,
+                                }}>
+                                    <h3 className="text-white text-xl sm:text-4xl tracking-[-0.04em] line-clamp-2 mb-1 leading-[100%]">{event.title}</h3>
+                                    <p className="text-white arial text-sm sm:text-base">{event.date.toLocaleDateString()}</p>
+                                </SVGFilter>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
