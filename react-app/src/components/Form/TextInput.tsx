49e887c5bffe002fe8c407455ed34c97a6d6f7f6
CoopCodes <104402442+CoopCodes@users.noreply.github.com>
2026-01-03 15:55:09 +1000

    Forogot to commit ts

--- react-app/src/components/Form/TextInput.tsx
+++ react-app/src/components/Form/TextInput.tsx
@@ -8,7 +8,7 @@ interface TextInputProps {
 export function TextInput({ label, mandatory = false, inputClassName = 'w-full md:w-[300px]', type = 'text' }: TextInputProps) {
     return (
         <div className="flex gap-4">
-            <label className="p1 whitespace-nowrap">{label}{mandatory ? "*" : ''}</label>
+            <label className="p1 whitespace-nowrap">{label}{mandatory ? "" : ''}</label>
             <input type={type} className={`border-b-2 bg-none outline-none p1 ${inputClassName}`} />
         </div>
     )
